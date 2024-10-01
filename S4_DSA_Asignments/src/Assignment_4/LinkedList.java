41e43d33160d1f250eb523a14161476480ad2427
RodneyCStead <RodCStead@gmail.com>
2024-09-30 22:44:32 -0230

    Added the delete method for the single linked List, tested inside of Main class to ensure it works. Task completed

--- S4_DSA_Asignments/src/Assignment_4/LinkedList.java
+++ S4_DSA_Asignments/src/Assignment_4/LinkedList.java
@@ -6,7 +6,8 @@ public class LinkedList {
     public Node tail; // the end of the list
     public int size;
 
-    public Node createLinkedList(int nodeValue) {
+    public Node createLinkedList(int nodeValue)
+    {
         head = new Node();
         Node node = new Node();
         node.value = nodeValue;
@@ -16,14 +17,14 @@ public class LinkedList {
         size = 1;
         return head;
     }
+
     // insert into a linked list
     // 1. If linked list does not exsist, then create it
     // 2. Inserting at the start of the linked list
     // 3. Inserting at the end of the linked list
     // 4. Inserting into any location between the start and end of the linked list
-
-    public void insertInLinkedList(int nodeValue, int location) {
-
+    public void insertInLinkedList(int nodeValue, int location)
+    {
         Node node = new Node();
         node.value = nodeValue;
         if (head == null) {
@@ -49,7 +50,8 @@ public class LinkedList {
         size++;
     }
     //Traverse a linked list
-    public void traverseLinkedList() {
+    public void traverseLinkedList()
+    {
         if (head == null) {
             System.out.println ("Linked list does not exist");
         } else {
@@ -64,8 +66,10 @@ public class LinkedList {
         }
         System.out.print ("\n");
     }
+
     //Search for a node in a linked list
-    public boolean searchNode(int nodeValue) {
+    public boolean searchNode(int nodeValue)
+    {
         if (head != null) {
             Node tempNode = head;
             for (int i = 0; i < size; i++) {
@@ -80,6 +84,54 @@ public class LinkedList {
         return false;
     }
 
-    //Deleteing a node from a linked list
+    public void deleteNode(int location)
+    {
+
+        // Checking if the link exists
+        if (head == null) {
+            System.out.println("The linked list does not exist");
+            return;
+        }
+
+        // Deleting the first node
+        if (location == 0) {
+            int deletedValue = head.value;
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = null;
+            }
+            System.out.println("Node with value " + deletedValue + " has been successfully deleted at location: " + location);
+        }
+        // Deleting the last node
+        else if (location >= size) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            int deletedValue = tempNode.next.value;
+            if (tempNode == head) {
+                head = tail = null;
+                size--;
+                System.out.println("Node with value " + deletedValue + " has been successfully deleted at location: " + location);
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+            System.out.println("Node with value " + deletedValue + " has been successfully deleted at location: " + location);
+        }
+        // Deleting a node from any location between the start and end of the linked list
+        else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            int deletedValue = tempNode.next.value;
+            tempNode.next = tempNode.next.next;
+            size--;
+            System.out.println("Node with value " + deletedValue + " has been successfully deleted at location: " + location);
+        }
+    }
 
 }
--- S4_DSA_Asignments/src/Assignment_4/Main.java
+++ S4_DSA_Asignments/src/Assignment_4/Main.java
@@ -12,8 +12,18 @@ public class Main {
         sda.insertInLinkedList(5,4);
         sda.insertInLinkedList(6,5);
 
+        System.out.println("Testing traverseLinkedList method");
         sda.traverseLinkedList();
+
+        System.out.println("");
+        System.out.println("Testing searchNode method");
         sda.searchNode(6); // expected to return true
         sda.searchNode(7); // expected to return false
+
+        System.out.println("");
+        System.out.println("Testing deleteNode method");
+        sda.deleteNode(0);
+        sda.traverseLinkedList();
+
     }
 }
