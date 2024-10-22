0ce36b86d35e4628f0a6db9180e1f2ef46f72f44
RodneyCStead <RodCStead@gmail.com>
2024-10-22 20:17:16 -0230

    Added base classes for the assignment

---
+++ S4_DSA_Asignments/src/Assignment_6/Animal.java
@@ -0,0 +1,19 @@
+package Assignment_6;
+
+public abstract class Animal {
+    private String name;
+    private long timestamp;
+
+    public Animal (String name) {
+        this.name = name;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
---
+++ S4_DSA_Asignments/src/Assignment_6/Dog.java
@@ -0,0 +1,7 @@
+package Assignment_6;
+
+public class Dog extends Animal {
+    public Dog (String name) {
+        super(name);
+    }
+}
